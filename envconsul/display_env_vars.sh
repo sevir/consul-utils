edda2d338769c40a6e4161f7b16d8192be1a5e27
José Fco. Rives <jose@digio.es>
2019-08-07 18:32:51 +0200

    example for envconsul

---
+++ envconsul/display_env_vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true
+do 
+    date
+    env
+    sleep 2
+done
---
+++ envconsul/download_binary_release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://releases.hashicorp.com/envconsul/0.8.0/envconsul_0.8.0_linux_386.tgz
+tar xvzf envconsul_0.8.0_linux_386.tgz
+rm envconsul_0.8.0_linux_386.tgz
